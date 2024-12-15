3395a63f1063197abd079c234ce2e5e59f748186
Michael von Riegen <michael@vonriegen.de>
2024-12-15 23:04:06 +0100

    chore: update barbar, bufferline config

---
+++ lua/config/barbar.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd('WinClosed', {
+  callback = function(tbl)
+    if tbl.args ~= nil then
+      vim.api.nvim_command('BufferClose ' .. tbl.args)
+    end
+  end,
+  group = vim.api.nvim_create_augroup('barbar_close_buf', {})
+})
--- lua/config/bufferline.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_autocommand('WinClosed', {
-    callback = function(tbl)
-      vim.api.nvim_command('BufferClose ' .. tbl.buf)
-    end,
-    group = vim.api.nvim_create_augroup('barbar_close_buf', {})
-  })
