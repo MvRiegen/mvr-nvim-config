af0bff7ee3c6863a256b4b94ed2c0c40f153abc3
Michael von Riegen <michael@vonriegen.de>
2024-12-15 22:16:22 +0100

    chore: update bufferline, telescope plugin config; update bufferline config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,9 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+  }
 }
