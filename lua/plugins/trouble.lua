acc25ae6b201f8c84d261f267efdfb048bbc3bf6
Michael von Riegen <michael@vonriegen.de>
2025-12-15 21:42:54 +0100

    feat: add trouble plugin

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/trouble.nvim",
+  version = "*",
+  cmd = "Trouble",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+    { "<leader>xw", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+    { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+    { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+  },
+  opts = {},
+}
