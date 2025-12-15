54cd8017b24192396f997d6a2fd9c88214b68e93
Michael von Riegen <michael@vonriegen.de>
2025-12-15 21:50:55 +0100

    chore: update keymaps config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,13 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
 vim.keymap.set('n', '<leader>fd', builtin.buffers, { desc = "Buffers" })
+
+-- Quick quit: close NvimTree if open, then quit all
+local function smart_quit()
+  local ok, api = pcall(require, "nvim-tree.api")
+  if ok then
+    api.tree.close()
+  end
+  vim.cmd("qa")
+end
+vim.keymap.set('n', '<leader>qq', smart_quit, { desc = "Quit all (close tree)" })
