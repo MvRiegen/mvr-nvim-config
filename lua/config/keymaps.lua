a8fda11759266d92c287c95d6fed712c459724e0
Michael von Riegen <michael@vonriegen.de>
2025-12-15 21:46:44 +0100

    chore: update keymaps config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,4 +9,3 @@ vim.keymap.set("n","<leader>c",":NvimTreeClose<CR>", { noremap = true, silent = 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
-vim.keymap.set('n', '<leader>fd', builtin.buffers, { desc = "Buffers" })
