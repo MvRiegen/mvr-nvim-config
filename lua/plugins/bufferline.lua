49a919c9a595ae9f8283a2012fa361aa45b6a1c9
Michael von Riegen <michael@vonriegen.de>
2025-12-15 21:03:43 +0100

    feat: add bufferline plugin

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,21 @@
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  event = "VeryLazy",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      diagnostics = "nvim_lsp",
+      separator_style = "slant",
+      offsets = { { filetype = "NvimTree", text = "File Explorer", separator = true } },
+    },
+  },
+  keys = {
+    { "<S-l>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next buffer" },
+    { "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous buffer" },
+    { "<leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>", desc = "Buffer 1" },
+    { "<leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>", desc = "Buffer 2" },
+    { "<leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>", desc = "Buffer 3" },
+    { "<leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>", desc = "Buffer 4" },
+  },
+}
